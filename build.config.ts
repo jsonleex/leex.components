312fcdc71f7e435eb892984659b3c9431892d16e
leex <jsonleex@163.com>
2023-09-08 19:18:07 +0800

    chore: update build config

--- build.config.ts
+++ build.config.ts
@@ -3,13 +3,21 @@ import { type BuildEntry, defineBuildConfig } from 'unbuild'
 const entry: BuildEntry = {
   builder: 'mkdist',
   input: './components',
-  pattern: ['**/*.{ts,vue,css}', '!_template', '!**/demos'],
+  pattern: ['!_template', '!**/demos'], // ignores
 }
 
 export default defineBuildConfig({
   entries: [
-    { ...entry, format: 'esm' },
-    { ...entry, format: 'cjs' },
+    // *.css
+    { ...entry, pattern: ['**/*.css', ...entry.pattern], loaders: ['sass'] },
+
+    // *.vue -> *.vue
+    // to avoid `<script lang='ts'>` -> `<script>`, only use `vue` loader for *.vue
+    { ...entry, pattern: ['**/*.vue', ...entry.pattern], loaders: ['vue'] },
+
+    // *.ts -> *.js & *.mjs
+    { ...entry, format: 'cjs', pattern: ['**/*.ts', ...entry.pattern], loaders: ['js'] },
+    { ...entry, format: 'esm', pattern: ['**/*.ts', ...entry.pattern], loaders: ['js'] },
   ],
   clean: true,
   declaration: true,
